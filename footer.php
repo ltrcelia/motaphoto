feff292da89268bc03a8a46b2d1acb59e9a3e1a7
Celia <celia.letellier@gmail.com>
2024-03-05 17:52:56 +0100

    Ajout header et footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <ul>
+        <li><a href="#mentions-legales">MENTIONS LÉGALES</a></li>
+        <li><a href="#vie-privee">VIE PRIVÉE </a></li>
+        <li><a href="#tous-droits-reserves">TOUS DROITS RÉSERVÉS</a></li>
+    </ul>
+</footer>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script defer src="assets/js/script.js"></script>
+	<link rel="stylesheet" href="assets/css/style.css"/>
+</head>
+
+<body>
+	<header>
+		<nav>
+            <img src="assests/img/Logo.png" alt="Logo motaphoto">
+            <ul>
+                <li><a href="#acceuil">ACCUEIL</a></li>
+                <li><a href="#a-propos">À PROPOS</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+			</ul>
+		</nav>
+	</header>
