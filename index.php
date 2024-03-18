7b85f0ddfd3a71fed48877e8b73ed41e2349b822
Celia <celia.letellier@gmail.com>
2024-03-18 13:06:23 +0100

    Ajout hero header

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 get_header();
 ?>
 
+<main>
+
+    <section class="hero-header">
+        <img id="hero" src="<?php echo get_template_directory_uri() ?>/assets/img/nathalie-11.jpeg" alt="Pleins de personnes rassemblées pour un évenement">
+        <h1>photographe event</h1>
+    </section>
+
+</main>
+
+
 <?php
 get_footer();
 ?>
